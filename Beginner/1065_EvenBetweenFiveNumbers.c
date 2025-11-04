7e2e97e72ae587ae0dc915a0fee53db8d1c27f2f
Shumsuddin Muzahid <125088463+muzahid003187@users.noreply.github.com>
2025-11-04 15:34:20 +0600

    Implement Even Numbers Between Five Numbers

---
+++ Beginner/1065_EvenBetweenFiveNumbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+
+    int a, b, c, d, e;
+    scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e);
+    int count = 0;
+    if (a % 2 == 0)
+    {
+        count++;
+    }
+    if (b % 2 == 0)
+    {
+        count++;
+    }
+    if (c % 2 == 0)
+    {
+        count++;
+    }
+    if (d % 2 == 0)
+    {
+        count++;
+    }
+    if (e % 2 == 0)
+    {
+        count++;
+    }
+
+    printf("%d valores pares\n", count);
+    return 0;
+}
