ef285d98886937296cf35fb9cbb66886be3a8c31
Shumsuddin Muzahid <125088463+muzahid003187@users.noreply.github.com>
2025-11-04 15:29:25 +0600

    Implement Average 2 calculation

---
+++ Beginner/1006_Average2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    double media;
+
+    media = (a*2 + b*3 + c*5)/10;
+
+
+    printf("MEDIA = %.1lf\n", media);
+
+    return 0;
+}
